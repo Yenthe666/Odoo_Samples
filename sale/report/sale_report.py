3efe0a367affa5235a34466e22f7d08aacd0f62e
Yenthe <yenthevg@gmail.com>
2015-04-10 13:18:31 +0200

    Sample Many2one
    Changes in sale.py and sale_view.xml

---
+++ sale/report/sale_report.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import tools
+from openerp.osv import fields, osv
+
+class sale_report(osv.osv):
+    _name = "sale.report"
+    _description = "Sales Orders Statistics"
+    _auto = False
+    _rec_name = 'date'
+
+    _columns = {
+        'date': fields.datetime('Date Order', readonly=True),  # TDE FIXME master: rename into date_order
+        'date_confirm': fields.date('Date Confirm', readonly=True),
+        'product_id': fields.many2one('product.product', 'Product', readonly=True),
+        'product_uom': fields.many2one('product.uom', 'Unit of Measure', readonly=True),
+        'product_uom_qty': fields.float('# of Qty', readonly=True),
+
+        'partner_id': fields.many2one('res.partner', 'Partner', readonly=True),
+        'company_id': fields.many2one('res.company', 'Company', readonly=True),
+        'user_id': fields.many2one('res.users', 'Salesperson', readonly=True),
+        'price_total': fields.float('Total Price', readonly=True),
+        'delay': fields.float('Commitment Delay', digits=(16,2), readonly=True),
+        'categ_id': fields.many2one('product.category','Category of Product', readonly=True),
+        'nbr': fields.integer('# of Lines', readonly=True),  # TDE FIXME master: rename into nbr_lines
+        'state': fields.selection([
+            ('draft', 'Quotation'),
+            ('waiting_date', 'Waiting Schedule'),
+            ('manual', 'Manual In Progress'),
+            ('progress', 'In Progress'),
+            ('invoice_except', 'Invoice Exception'),
+            ('done', 'Done'),
+            ('cancel', 'Cancelled')
+            ], 'Order Status', readonly=True),
+        'pricelist_id': fields.many2one('product.pricelist', 'Pricelist', readonly=True),
+        'analytic_account_id': fields.many2one('account.analytic.account', 'Analytic Account', readonly=True),
+        'section_id': fields.many2one('crm.case.section', 'Sales Team'),
+    }
+    _order = 'date desc'
+
+    def _select(self):
+        select_str = """
+             SELECT min(l.id) as id,
+                    l.product_id as product_id,
+                    t.uom_id as product_uom,
+                    sum(l.product_uom_qty / u.factor * u2.factor) as product_uom_qty,
+                    sum(l.product_uom_qty * l.price_unit * (100.0-l.discount) / 100.0) as price_total,
+                    count(*) as nbr,
+                    s.date_order as date,
+                    s.date_confirm as date_confirm,
+                    s.partner_id as partner_id,
+                    s.user_id as user_id,
+                    s.company_id as company_id,
+                    extract(epoch from avg(date_trunc('day',s.date_confirm)-date_trunc('day',s.create_date)))/(24*60*60)::decimal(16,2) as delay,
+                    s.state,
+                    t.categ_id as categ_id,
+                    s.pricelist_id as pricelist_id,
+                    s.project_id as analytic_account_id,
+                    s.section_id as section_id
+        """
+        return select_str
+
+    def _from(self):
+        from_str = """
+                sale_order_line l
+                      join sale_order s on (l.order_id=s.id)
+                        left join product_product p on (l.product_id=p.id)
+                            left join product_template t on (p.product_tmpl_id=t.id)
+                    left join product_uom u on (u.id=l.product_uom)
+                    left join product_uom u2 on (u2.id=t.uom_id)
+        """
+        return from_str
+
+    def _group_by(self):
+        group_by_str = """
+            GROUP BY l.product_id,
+                    l.order_id,
+                    t.uom_id,
+                    t.categ_id,
+                    s.date_order,
+                    s.date_confirm,
+                    s.partner_id,
+                    s.user_id,
+                    s.company_id,
+                    s.state,
+                    s.pricelist_id,
+                    s.project_id,
+                    s.section_id
+        """
+        return group_by_str
+
+    def init(self, cr):
+        # self._table = sale_report
+        tools.drop_view_if_exists(cr, self._table)
+        cr.execute("""CREATE or REPLACE VIEW %s as (
+            %s
+            FROM ( %s )
+            %s
+            )""" % (self._table, self._select(), self._from(), self._group_by()))
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
