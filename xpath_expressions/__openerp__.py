01c172f3ff4e502fd458e76f35f1e10ff9653da8
Yenthe <yenthevg@gmail.com>
2015-04-10 15:25:41 +0200

    xpath sample module
    This module shows you how to create xpath expressions to create a new
    page, new group or new fields.

---
+++ xpath_expressions/__openerp__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "xpath_expressions",
+
+    'summary': """
+        xpath examples""",
+
+    'description': """
+        This module shows some xpath examples under sale > products.
+	It has options to create a new tab, a new group and new fields.
+    """,
+
+    'author': "Yenthe Van Ginneken",
+    'website': "http://www.odoo.yenthevg.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['sale'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo.xml',
+    ],
+}
