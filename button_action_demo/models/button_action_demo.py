c3605b7d7c5076478764c536992d3dbcf26ac07d
Yenthe <yenthevg@gmail.com>
2016-01-10 10:00:15 +0100

    Add @api.one warning
    Add @api.one warning

--- button_action_demo/models/button_action_demo.py
+++ button_action_demo/models/button_action_demo.py
@@ -10,17 +10,20 @@ class button_action_demo(models.Model):
     _name = 'button.demo'
     name = fields.Char(required=True,default='Click on generate name!')
     password = fields.Char()
-
+    
+    # WARNING! @api.one is deprecated in Odoo 9, use @api.multi with ensure_one instead!
     @api.one
     def generate_record_name(self):
 	#Generates a random name between 9 and 15 characters long and writes it to the record.
 	self.write({'name': ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(randint(9,15)))})
 
+    # WARNING! @api.one is deprecated in Odoo 9, use @api.multi with ensure_one instead!
     @api.one
     def generate_record_password(self):
 	#Generates a random password between 12 and 15 characters long and writes it to the record.
 	self.write({'password': ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(randint(12,15)))})
 
+    # WARNING! @api.one is deprecated in Odoo 9, use @api.multi with ensure_one instead!
     @api.one
     def clear_record_data(self):
 	self.write({
