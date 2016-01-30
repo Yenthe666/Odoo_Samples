553ae3bdcf8bd0cfe126c986504869f1500e75df
Yenthe <yenthevg@gmail.com>
2016-01-30 14:51:37 +0100

    [Fix] Incorrect indentation
    Accidently added too many spaces.

--- button_action_demo/models/button_action_demo.py
+++ button_action_demo/models/button_action_demo.py
@@ -13,20 +13,22 @@ class button_action_demo(models.Model):
     
     @api.multi
     def generate_record_name(self):
-		self.ensure_one()
-	    #Generates a random name between 9 and 15 characters long and writes it to the record.
-	    self.write({'name': ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(randint(9,15)))})
+	self.ensure_one()
+	#Generates a random name between 9 and 15 characters long and writes it to the record.
+	self.write({'name': ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(randint(9,15)))})
 
     @api.multi
     def generate_record_password(self):
-		self.ensure_one()
-	    #Generates a random password between 12 and 15 characters long and writes it to the record.
-	    self.write({'password': ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(randint(12,15)))})
+	self.ensure_one()
+	#Generates a random password between 12 and 15 characters long and writes it to the record.
+	self.write({
+	    'password': ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(randint(12,15)))
+	})
 
     @api.multi
     def clear_record_data(self):
-		self.ensure_one()
-	    self.write({
-	        'name': '',
-	        'password': ''
-	    })
+	self.ensure_one()
+	self.write({
+	    'name': '',
+	    'password': ''
+	})
