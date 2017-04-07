b3e95a1223d90569f70281914dd37ec8c970d54e
Yenthe <yenthevg@gmail.com>
2017-04-07 13:50:19 +0200

    [REM] All old tutorials - start with 10.0 modules
    This will be used to know which modules (might be) are in old API and
    which are 10.0 (meaning new API)

--- ErpPeekDemo/install_module.py
+++
@@ -1,9 +0,0 @@
-import erppeek
-# You can use this client if you have Erppeek installed and have a erppeek.ini file
-# client = erppeek.Client.from_config('ErpPeekDemoDatabase')
-# The alternative is by specifying the settings by command
-client = erppeek.Client('http://localhost:8080', 'ErpPeekDemoDatabase', 'admin', 'admin')
-
-modules = client.modules('sale', installed=False)
-if 'sale' in modules['uninstalled']:
-    client.install('sale')
--- scheduler_demo/models/__init__.py
+++
@@ -1,1 +0,0 @@
-import scheduler_model
